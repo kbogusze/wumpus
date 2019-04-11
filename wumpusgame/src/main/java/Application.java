4c9bd6c65c31fcc79ba6e4aa9e8b89b1f78dda21
kbogusze <kbogusze@gmail.com>
2019-04-11 13:34:30 +0200

    Initial Configuration

---
+++ wumpusgame/src/main/java/Application.java
@@ -0,0 +1,10 @@
+import Controller.GameController;
+
+public class Application {
+
+    public static void main(String[] args) {
+        GameController controller = new GameController();
+        controller.initializeGame();
+    }
+
+}
