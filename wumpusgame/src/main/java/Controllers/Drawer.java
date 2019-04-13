be6462d38e91e6b656f68df9e5e206897ee79f49
kbogusze <kbogusze@gmail.com>
2019-04-13 07:37:25 +0200

    Implementation of Keyboard Events

--- wumpusgame/src/main/java/Controllers/Drawer.java
+++ wumpusgame/src/main/java/Controllers/Drawer.java
@@ -11,28 +11,37 @@ public class Drawer {
     static final String STATIC_CONTROL_PATH = "background/control";
     static final String STATIC_INFO_PATH = "background/info";
 
-    public void drawFile(String path ,TextArea textArea){
+    private TextArea textArea;
+
+    public Drawer(TextArea textArea) {
+        this.textArea = textArea;
+    }
+
+    public void drawFile(String path){
         InputStream is = getClass().getClassLoader().getResourceAsStream(path);
         if (is != null) {
             BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-            drawFromStream(reader, textArea);
+            drawFromStream(reader);
         }
     }
 
-    public void drawHeader(TextArea textArea) {
-        drawFile(STATIC_HEADER_PATH, textArea);
+    public void drawHeader() {
+        drawFile(STATIC_HEADER_PATH);
     }
 
-    public void drawInstruction(TextArea textArea) {
-        drawFile(STATIC_CONTROL_PATH, textArea);
+    public void drawInstruction() {
+        drawFile(STATIC_CONTROL_PATH);
     }
 
-    public void drawFromStream(BufferedReader reader, TextArea textArea){
+    public void drawFromStream(BufferedReader reader){
         reader.lines().forEach(s -> textArea.appendText(s + "\n"));
     }
 
-    public void drawInfo(TextArea textArea) {
-        drawFile(STATIC_INFO_PATH, textArea);
+    public void drawInfo() {
+        drawFile(STATIC_INFO_PATH);
     }
 
+    public void drawLevel(int level, int points, int arrowsNumber) {
+        textArea.appendText("LEVEL: " + level + "  POINTS: " + points + " ARROWS: " + arrowsNumber + "\n");
+    }
 }
--- wumpusgame/src/main/java/Controllers/GameController.java
+++ wumpusgame/src/main/java/Controllers/GameController.java
@@ -3,6 +3,7 @@ package Controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import models.GameObject;
 import models.Player;
@@ -15,6 +16,7 @@ public class GameController implements Initializable {
 
     private int level = 1;
     private int points = 0;
+    private int arrowsNumber = 0;
     private Board board;
     private Drawer drawer;
     private Player player;
@@ -25,33 +27,71 @@ public class GameController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        this.drawer = new Drawer();
+        this.drawer = new Drawer(textArea);
         this.player = new Player();
         this.audioPlayer = new AudioPlayer();
         createBoard();
         audioPlayer.playIntro();
+        drawBackground();
         drawGame();
     }
 
     @FXML
     void keyboardEvent(KeyEvent event) {
+        audioPlayer.playPip();
+        if (event.getCode() == KeyCode.W) {
+            moveForward();
+        } else if (event.getCode() == KeyCode.A) {
+            turnLeft();
+        } else if (event.getCode() == KeyCode.D) {
+            turnRight();
+        }else if (event.getCode() == KeyCode.SPACE) {
+            shootArrow();
+        } else if (event.getCode() == KeyCode.I) {
+            drawInfo();
+        }
+    }
+
+    private void drawInfo() {
+        drawer.drawInfo();
+    }
+
+    private void shootArrow() {
+
+    }
+
+    private void turnRight() {
+        player.turnRigh();
+        drawGame();
+    }
+
+    private void turnLeft() {
+        player.turnLeft();
+        drawGame();
+    }
+
+    private void moveForward() {
 
     }
 
     public void drawGame(){
-        drawBackground();
+        drawLevel();
         drawTips();
         drawPlayer();
         drawInstruction();
     }
 
+    private void drawLevel() {
+        drawer.drawLevel(level, points, arrowsNumber);
+    }
+
     private void drawTips() {
         List<GameObject> objects = board.getTipsInCurrentCell();
-        objects.forEach(d -> drawer.drawFromStream(d.draw(),textArea));
+        objects.forEach(d -> drawer.drawFromStream(d.draw()));
     }
 
     private void drawPlayer() {
-        drawer.drawFromStream(player.draw(),textArea);
+        drawer.drawFromStream(player.draw());
     }
 
     private void createBoard(){
@@ -61,13 +101,11 @@ public class GameController implements Initializable {
     }
 
     private void drawBackground() {
-        drawer.drawHeader(textArea);
+        drawer.drawHeader();
     }
 
     private void drawInstruction() {
-        drawer.drawInstruction(textArea);
+        drawer.drawInstruction();
     }
-
-
 
 }
